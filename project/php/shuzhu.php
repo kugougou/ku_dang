a41bbcfb89193c52ed359a62ef0728943d686a70
kugougou <1986245110@qq.com>
2020-03-20 20:39:28 +0800

    我提交了项目

---
+++ project/php/shuzhu.php
@@ -0,0 +1,18 @@
+<?php
+header('content-type:text/html;charset=utf-8');
+define('HOST', 'localhost');
+define('ROOT', 'root');
+define('POSS', '');
+define('DBNAME', 'peixun');
+
+$coon = @new mysqli(HOST, ROOT, POSS, DBNAME);
+if ($coon->connect_errno) {
+    die('数据库连接错误，请检查' . $coon->connect_errno);
+};
+mysqli_set_charset($coon, 'utf8');
+$res =  $coon->query("select * from arerr");
+$arr = array();
+for ($i = 0; $i < ($res->num_rows); $i++) {
+    $arr[$i] = $res->fetch_assoc();
+};
+echo json_encode($arr);
